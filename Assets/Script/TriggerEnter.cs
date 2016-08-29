341401fadc90163b84035e6a75d469df72bbe0bb
jasonwu <jasonwu950709@gmail.com>
2016-08-29 17:05:10 +1000

    add simple testing

---
+++ Assets/Script/TriggerEnter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerEnter : MonoBehaviour {
+    Vector3 pos;
+
+	// Use this for initialization
+	void Start () {
+        pos = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter (Collider other)
+    {
+        pos.x = pos.x + 2;
+        gameObject.transform.position = pos;
+    }
+}
