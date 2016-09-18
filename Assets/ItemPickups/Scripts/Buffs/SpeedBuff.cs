57aa34ef013a06cbb52905cfcd59d5df2b526dee
JiaYi Bai <JiaYi Bai>
2016-09-18 15:29:04 +1000

    speedbuff and itemSpawn demo

---
+++ Assets/ItemPickups/Scripts/Buffs/SpeedBuff.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// created by Jia Yi Bai, jiab1@student.unimelb.edu.au
+// speed boost buff
+public class SpeedBuff : Buff {
+	//speedup will last for 5 seconds with 100% increase speed
+	private float durationTime = 5.0f;
+	private float speedBoostRatio = 1.0f;
+
+	public override void UpdateBuff (Player player){
+		this.ReduceTime ();
+		if (this.IsExpired ()) {
+			FinishBuff (player);
+		}
+	}
+
+	public override void InitialiseBuff(Player player){
+		// buff initiallise with a new duration
+		this.SetDuration (durationTime);
+		this.ResetTime();
+		// modify player's speedratio
+		player.speedRatio += speedBoostRatio;
+	}
+
+	public override void FinishBuff(Player player){
+		player.speedRatio -= speedBoostRatio;
+	}
+}
---
+++ Assets/ItemPickups/Scripts/InstantEffect.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// created by Jia Yi Bai, jiab1@student.unimelb.edu.au
+
+public class InstantEffect : ItemEffect {
+
+	// Update is called once per frame
+	public void EffectOn(Player player) {
+		
+	}
+}
